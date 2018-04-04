56a548ada6dff58507886faa5404ba9d86073ae6
akshaydashore <ad12456>
2018-04-04 22:08:13 +0530

    test

--- app/src/main/java/infobite/technology/info/eganacsi/activity/MyTest.java
+++ app/src/main/java/infobite/technology/info/eganacsi/activity/MyTest.java
@@ -1,8 +1,18 @@
 package infobite.technology.info.eganacsi.activity;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by kamlesh on 4/4/2018.
  */
+public class MyTest extends AppCompatActivity{
 
-public class MyTest {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
