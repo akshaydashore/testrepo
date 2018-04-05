53f5b82213afaf3a44086bd08a2187575d93dff0
akshaydashore <31306992+akshaydashore@users.noreply.github.com>
2018-04-05 21:59:06 +0530

    first push

--- app/src/main/java/infobite/technology/info/eganacsi/activity/MyTest.java
+++ app/src/main/java/infobite/technology/info/eganacsi/activity/MyTest.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by kamlesh on 4/4/2018.
@@ -14,5 +15,6 @@ public class MyTest extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e( "onCreate: ","test" );
     }
 }
